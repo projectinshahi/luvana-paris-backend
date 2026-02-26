f1d954f5630c4f95088f0b4c5b105572317b431a
Anas M K <anasmk@elancelearning.com>
2026-02-26 14:39:37 +0530

    implementing cloudinary

--- routes/admin.js
+++ routes/admin.js
@@ -35,4 +35,7 @@ router.use('/order', require('./admin/orderRoute'));
 // Use users route
 router.use('/customer', require('./admin/userRoute'));
 
+// Image upload route
+router.use('/general', require('./admin/generalRoute'));
+
 module.exports = router;
